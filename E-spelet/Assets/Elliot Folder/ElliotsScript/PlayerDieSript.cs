404e07422e69a7a7b146ff278a72eba49946f6b5
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2020-04-28 20:31:37 +0200

    spelas bara en gång

--- E-spelet/Assets/Elliot Folder/ElliotsScript/PlayerDieSript.cs
+++ E-spelet/Assets/Elliot Folder/ElliotsScript/PlayerDieSript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerDieSript : MonoBehaviour
 {
     public ParticleSystem DeathEffeckt;
+    bool playOnce = false;
     // Update is called once per frame
     void Update()
     {
-        if(GetComponent<PlayerClockSript>().Endgame)
+        if(GetComponent<PlayerClockSript>().Endgame && !playOnce)
         {
+            playOnce = true;
             Instantiate(DeathEffeckt, transform.position, Quaternion.identity);
             Arcade.SetScore(GetComponent<PlayerMovementPrototype>().Id, Mathf.RoundToInt(ScoreCounterScript.Sec));
             Destroy(this.gameObject,.5f);
