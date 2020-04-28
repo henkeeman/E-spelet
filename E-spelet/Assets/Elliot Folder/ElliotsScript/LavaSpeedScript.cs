68f3dd05b1392be4757eefc72f3300a2a0e6f78a
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2020-04-28 20:45:21 +0200

    Lava speed

--- E-spelet/Assets/Elliot Folder/ElliotsScript/LavaSpeedScript.cs
+++ E-spelet/Assets/Elliot Folder/ElliotsScript/LavaSpeedScript.cs
@@ -6,7 +6,7 @@ public class LavaSpeedScript : MonoBehaviour
 {
     public float Mass;
     public float PullForse;
-
+    public float MaxSpeed = 5;
 
     // Update is called once per frame
     void Update()
@@ -21,8 +21,8 @@ public class LavaSpeedScript : MonoBehaviour
         transform.position -= new Vector3(0, ((Mass * PullForse) * Time.deltaTime), 0);
         //print(transform.position.y);
         if (transform.position.y > 100)
-            PullForse = 5;
+            PullForse = MaxSpeed;
         else 
-        PullForse = Mathf.Lerp(1, 5, transform.position.y / 100);// Kändes wierd
+        PullForse = Mathf.Lerp(1, MaxSpeed, transform.position.y / 100);// Kändes wierd
     }
 }
