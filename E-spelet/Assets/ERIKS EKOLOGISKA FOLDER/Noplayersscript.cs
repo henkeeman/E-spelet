1b6c7792065d1fabb1df530713365a34f6f0c2f3
Ciderboy27 <48049674+Ciderboy27@users.noreply.github.com>
2020-04-29 12:46:11 +0200

    Fixed back to menu

---
+++ E-spelet/Assets/ERIKS EKOLOGISKA FOLDER/Noplayersscript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Media;
+using UnityEngine;
+
+
+public class Noplayersscript : MonoBehaviour
+{
+
+    // Update is called once per frame
+
+    float time = 0;
+    public float StartTime = 2;
+
+
+    void LateUpdate()
+    {
+
+        if (time > StartTime)
+        {
+            if (GetComponent<CameraSmootherScript>().targets == null)
+                return;
+
+            else if (GetComponent<CameraSmootherScript>().targets.Count == 0)
+
+            {
+
+                Application.LoadLevel("spelarmeny");
+            }
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+    }
+}
